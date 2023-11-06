2c65dd809886a645b087144fb319dbb33582f710
engadnanhossain <engr.adnanhossain@gmail.com>
2023-11-06 11:34:25 +0600

    add bcrypt

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const User = require('../model/userSchema');
+const bcrypt = require('bcrypt');
 const userRouter = express();
 
 userRouter.post('/user', async (req, res) => {
     try {
+        const { name, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const user = new User({
-            ...req.body
-        })
+            name,
+            email,
+            password: hashedPassword,
+        });
+
         const result = await user.save();
         res.status(200).json({
             success: true,
             data: result
-        })
+        });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-})
+});
+
 
 module.exports = userRouter;
