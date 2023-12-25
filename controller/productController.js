6cf1fc1504a198f5543e5b382dbbcbadaec9afe1
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-25 14:56:55 +0600

    add single product api

--- controller/productController.js
+++ controller/productController.js
@@ -18,13 +18,16 @@ const postProductController = async (req, res) => {
 // get product 
 const getProductController = async (req, res) => {
     try {
+        let totalProducts;
+        totalProducts = await Product.find().countDocuments();
         const product = await Product.find()
             .populate({ path: 'categories', select: 'category' })
             .populate({ path: 'subCategory', select: 'name' })
-            
+
         if (product) {
             res.status(200).json({
                 message: "success",
+                totalProducts,
                 data: product
             })
         } else {
@@ -35,7 +38,26 @@ const getProductController = async (req, res) => {
     }
 }
 
+
+// get single product 
+const getSingleProductController = async (req, res) => {
+    try {
+        const product = await Product.findOne({ _id: req.params.id });
+        if (product) {
+            res.status(200).json({
+                message: "Success",
+                data: product
+            })
+        } else {
+            res.status(404).json({ message: "product not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
     postProductController,
-    getProductController
+    getProductController,
+    getSingleProductController
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { postProductController, getProductController } = require('../controller/productController');
+const { postProductController, getProductController, getSingleProductController } = require('../controller/productController');
 const productRouter = express();
 
 // routes
 
 productRouter.post('/', postProductController);
 productRouter.get('/', getProductController);
+productRouter.get('/:id', getSingleProductController);
 
 module.exports = productRouter;
