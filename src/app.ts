3f113bddcf78367b68bb1ad74d66f2a2a5a4e0d9
adnanaotrek <adnanaotrek@gmail.com>
2024-12-19 18:13:16 +0600

    change status

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { routes } from './app/routes';
+import httpStatus from 'http-status';
 const app = express();
 
 
@@ -14,8 +15,10 @@ app.use('/api/v1', routes)
 app.get('/',async(req: Request, res: Response) => {
     try {
         res.status(200).json({
+            statusCode: httpStatus.OK,
             success: true,
-            message: 'Server Running'
+            message: 'Server Running',
+            author: 'Adnan Hossain'
         })
     } catch (error: any) {
         console.error('error', error);
